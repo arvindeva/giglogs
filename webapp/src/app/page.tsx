5dee523c515c76d01d2bc382b77b45fdcc11911f
Arvindeva Wibisono <arvindeva@gmail.com>
2024-01-07 11:12:09 +0100

    Revert off

--- webapp/src/app/page.tsx
+++ webapp/src/app/page.tsx
@@ -15,7 +15,7 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            By{' '}Some Retard
+            By{' '}Oops
           </a>
         </div>
       </div>
